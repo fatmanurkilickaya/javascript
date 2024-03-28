143ceb136ed72e166e49d3148eafd980e2202632
Fatma Nur Kılıçkaya <82950952+fatmanurkilickaya@users.noreply.github.com>
2024-03-28 20:16:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var kullanicilar = [
+    {email:"fatmanur@kayseri.edu.tr",sifre:"123"},
+    {email:"derya@kayseri.edu.tr",sifre:"12345"}
+]
+var tivitler = [
+    {email:"fatmanur@kayseri.edu.tr",tivit: "Bugün hava güzel"},
+    {email:"derya@kayseri.edu.tr",tivit:"saat geç oldu"}
+]
+var email = prompt("email ?")
+var sifre = prompt("sifre ?")
+function giris(){
+    if((email == kullanicilar[0].email && kullanicilar[0].sifre) 
+    ||(email == kullanicilar[1].email && kullanicilar[1].sifre)){
+        console.log(tivitler)
+    }else{
+        alert("kullanıcı adı veya şifresi hatalı!")
+    }
+}
+
+giris(email,sifre)
